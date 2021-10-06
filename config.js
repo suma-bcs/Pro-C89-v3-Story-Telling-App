3896994d2558e06b1df7f3779518481636d4a053
suma-bcs <76700432+suma-bcs@users.noreply.github.com>
2021-10-06 05:50:35 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDT4fXVufxt0ctAMb6c0nKMTJP7FWLCy0I",
+  authDomain: "storytelling-app-1718f.firebaseapp.com",
+  databaseURL: "https://storytelling-app-1718f-default-rtdb.firebaseio.com",
+  projectId: "storytelling-app-1718f",
+  storageBucket: "storytelling-app-1718f.appspot.com",
+  messagingSenderId: "425492908801",
+  appId: "1:425492908801:web:5e0e80aa5053464839a900"
+};
